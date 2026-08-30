1364a0b1ec0ab3330f3d13267793b52457ec5d6b
rarebuffalo <info.krishnasingh.codes@gmail.com>
2026-08-31 00:05:31 +0530

    Export ApiKey and Webhook models in app/models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,7 @@ from app.models.scan import ScanResult
 from app.models.code_scan import CodeScanResult
 from app.models.scheduled_scan import ScheduledScan
 from app.models.nuclei_result import NucleiScanResult
+from app.models.apikey import ApiKey
+from app.models.webhook import Webhook
 
-__all__ = ["User", "ScanResult", "CodeScanResult", "ScheduledScan", "NucleiScanResult"]
+__all__ = ["User", "ScanResult", "CodeScanResult", "ScheduledScan", "NucleiScanResult", "ApiKey", "Webhook"]
